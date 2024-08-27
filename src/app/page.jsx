1937c304d9fca91037d917102b50b1753334842d
Danylo Musiiaka <dollarone98@gmail.com>
2024-08-27 14:54:49 +0300

    created result page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,12 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { Suspense } from "react";
+
+export function Loader() {
+  return <div>Loading...</div>;
+}
 
 export default function VehicleSelector() {
   const [vehicleTypes, setVehicleTypes] = useState([]);
@@ -25,56 +30,71 @@ export default function VehicleSelector() {
   const isButtonDisabled = !selectedType || !selectedYear;
 
   const handleNextClick = () => {
+    const serchedVehicle = vehicleTypes.find(
+      (serchedVehicle) => serchedVehicle.MakeName === selectedType
+    );
+
     if (!isButtonDisabled) {
-      router.push(`/result/${selectedType}/${selectedYear}`);
+      router.push(`/result/${serchedVehicle.MakeId}/${selectedYear}`);
     }
   };
 
   return (
-    <form>
-      <h1>Select Your Vehicle</h1>
+    <form className='max-w-lg mx-auto p-6 bg-white rounded-lg shadow-md '>
+      <section>
+        <h1 className='text-2xl font-bold mb-6 text-center text-gray-800'>Select Your Vehicle</h1>
 
-      <div>
-        <label htmlFor='vehicleType'>Vehicle Type:</label>
-        <select
-          id='vehicleType'
-          value={selectedType}
-          onChange={(e) => setSelectedType(e.target.value)}
+        <div className='mb-4'>
+          <label htmlFor='vehicleType' className='block text-sm font-medium text-gray-700 mb-2'>
+            Vehicle Type:
+          </label>
+          <select
+            id='vehicleType'
+            value={selectedType}
+            onChange={(e) => setSelectedType(e.target.value)}
+            className='w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500'
+          >
+            <option value=''>Select a vehicle type</option>
+            {vehicleTypes.map((type) => (
+              <option key={type.MakeName} value={type.MakeName}>
+                {type.MakeName}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className='mb-6'>
+          <label htmlFor='modelYear' className='block text-sm font-medium text-gray-700 mb-2'>
+            Model Year:
+          </label>
+          <select
+            id='modelYear'
+            value={selectedYear}
+            onChange={(e) => setSelectedYear(e.target.value)}
+            className='w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500'
+          >
+            <option value=''>Select a model year</option>
+            {years.map((year) => (
+              <option key={year} value={year}>
+                {year}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <button
+          type='button'
+          onClick={handleNextClick}
+          className={`w-full py-2 px-4 rounded-lg font-semibold text-white transition-colors duration-300 ${
+            isButtonDisabled
+              ? "bg-gray-400 cursor-not-allowed"
+              : "bg-indigo-600 hover:bg-indigo-700"
+          }`}
+          disabled={isButtonDisabled}
         >
-          <option value=''>Select a vehicle type</option>
-          {vehicleTypes.map((type) => (
-            <option key={type.MakeName} value={type.MakeName}>
-              {type.MakeName}
-            </option>
-          ))}
-        </select>
-      </div>
-
-      <div>
-        <label htmlFor='modelYear'>Model Year:</label>
-        <select
-          id='modelYear'
-          value={selectedYear}
-          onChange={(e) => setSelectedYear(e.target.value)}
-        >
-          <option value=''>Select a model year</option>
-          {years.map((year) => (
-            <option key={year} value={year}>
-              {year}
-            </option>
-          ))}
-        </select>
-      </div>
-
-      <button
-        type='button'
-        onClick={handleNextClick}
-        style={{ marginTop: "20px" }}
-        className={`btn ${isButtonDisabled ? "btn-disabled" : "btn-primary"}`}
-        disabled={isButtonDisabled}
-      >
-        Next
-      </button>
+          Next
+        </button>
+      </section>
     </form>
   );
 }
